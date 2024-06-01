9e2ceb1fdc325381f5cdec53939be530f8f98057
DmitriiKovrigin <dimals18@icloud.com>
2024-06-01 10:51:36 +0300

    init

---
+++ petProjectMovies/petProjectMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  petProjectMoviesApp.swift
+//  petProjectMovies
+//
+//  Created by Dima Kovrigin on 01.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct petProjectMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
